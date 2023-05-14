b2fa2efe9905eaa3bb50e6c3c3c9a35d83ab5683
Ryoukaas <132536858+Ryoukaas@users.noreply.github.com>
2023-05-14 16:01:36 -0700

    Changed to ListActivity in Main Java added String Array Gadgets. Added image Asset (Untested or not working) for array. Added 2 of the 5 needed Array Indexes and set the list adapter.
    Also added Textview to code of activity_main.xml

--- app/src/main/java/com/example/techgadgetapps/MainActivity.java
+++ app/src/main/java/com/example/techgadgetapps/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.techgadgetapps;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] gadgets={"Nintendo Switch", "Steam Deck"};
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_main, R.id.gadget, gadgets));
     }
 }
