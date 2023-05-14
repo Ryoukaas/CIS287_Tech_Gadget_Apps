abdc75f059e6b19de2d87e8f37e8307784ef5904
Ryoukaas <132536858+Ryoukaas@users.noreply.github.com>
2023-05-14 16:35:50 -0700

    Changed to icon from hw files to try and solve imediate crash error. Added Steam Deck and Nintendo Switch Acitvities and changed their label in the AndroidManifest.xml.

---
+++ app/src/main/java/com/example/techgadgetapps/SteamDeck.java
@@ -0,0 +1,13 @@
+package com.example.techgadgetapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SteamDeck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_steam_deck);
+    }
+}
