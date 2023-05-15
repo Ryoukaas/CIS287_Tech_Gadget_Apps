a859db061cded6e0dc58c926ad2bff5a809a3543
Ryoukaas <132536858+Ryoukaas@users.noreply.github.com>
2023-05-14 17:23:12 -0700

    Got 2 of the 5 arrays working except for buttons, got the icon image fixed

--- app/src/main/java/com/example/techgadgetapps/MainActivity.java
+++ app/src/main/java/com/example/techgadgetapps/MainActivity.java
@@ -1,17 +1,41 @@
 package com.example.techgadgetapps;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        String[] gadgets={"Nintendo Switch", "Steam Deck"};
+        String[] gadgets = {"Nintendo Switch", "Steam Deck"};
         setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_main, R.id.gadget, gadgets));
+    }
+
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        switch (position) {
+            case 0:
+                startActivity(new Intent(MainActivity.this, NintendoSwitch.class));
+                break;
+
+            case 1:
+                startActivity(new Intent(MainActivity.this, SteamDeck.class));
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+        }
     }
 }
